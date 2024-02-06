459b38979c4d374dc75970cd8d5c9b98c29d9a73
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-07 01:51:12 +0530

    Refactored : checking venture server message v7

--- venture-srv/src/Framework/webserver/server.js
+++ venture-srv/src/Framework/webserver/server.js
@@ -3,7 +3,7 @@ const serverConfig=(server,config)=>{
     const startServer=()=>{
 
         server.listen(config.port,()=>{
-
+            console.log("checkin vneture serverg")
             console.log(`venture Service Listening on port ${config.port}🌈🌛`)
         })
 
