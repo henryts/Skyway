3c0c189495a8f5ce310a415465d7e14088287418
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-07 12:23:15 +0530

    Refactored : removed user-srv unwanted consoles v9

--- user-srv/src/Framework/webserver/server.js
+++ user-srv/src/Framework/webserver/server.js
@@ -3,7 +3,7 @@ const serverConfig=(server,config)=>{
     const startServer=()=>{
 
         server.listen(config.port,()=>{
-            console.log("server starting github actions perfect")
+        
             console.log(`user Service Listening on port ${config.port}`)
         })
 
