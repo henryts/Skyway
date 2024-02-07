e68460862946830982ec24c8940b736f6a103b0c
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-07 19:53:10 +0530

    Refactored: v21

--- chat-srv/src/config/env.ts
+++ chat-srv/src/config/env.ts
@@ -2,10 +2,10 @@ import env from "dotenv"
 env.config()
 export default {
 
-    PORT:process.env.PORT,
-    MONGO_URI:process.env.MONGO_URI,
-    USER_SRV_TOKEN_SECRET_KEY:process.env.USER_SRV_TOKEN_SECRET_KEY,
-    VENTURE_SRV_TOKEN_SECRET_KEY:process.env.VENTURE_SRV_TOKEN_SECRET_KEY,
+    PORT:process.env.PORT ?? 5003,
+    MONGO_URI:process.env.MONGO_URI ?? "mongodb+srv://ajmalmuhammed846:ajuZ3216@cluster0.uzyffxb.mongodb.net/skyway-chat-srv?retryWrites=true&w=majority" ,
+    USER_SRV_TOKEN_SECRET_KEY:process.env.USER_SRV_TOKEN_SECRET_KEY ?? "ajmal123user-srv" ,
+    VENTURE_SRV_TOKEN_SECRET_KEY:process.env.VENTURE_SRV_TOKEN_SECRET_KEY ?? "ajmal123venture-srv" ,
 
     
 
