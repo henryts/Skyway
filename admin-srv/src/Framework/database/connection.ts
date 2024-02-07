62f4e72d48a483b423a977df160f288e16b234b1
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-07 20:21:00 +0530

    Refactored: v25

--- admin-srv/src/Framework/database/connection.ts
+++ admin-srv/src/Framework/database/connection.ts
@@ -3,7 +3,7 @@ const connection = (uri: string) => {
 
    //connecting mongodb 
    mongoose.connect("mongodb+srv://ajmalmuhammed846:ajuZ3216@cluster0.uzyffxb.mongodb.net/skyway-admin-srv?retryWrites=true&w=majority")
-   //   mongoose.connect(uri)
+   //   mongoose.connect(uri) 
 
    mongoose.connection.once('open', () => {
 
