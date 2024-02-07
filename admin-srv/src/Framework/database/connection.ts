e97980966a711f501e31cb24c3ed8a3bd4aee59b
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-07 20:18:39 +0530

    Refactored: v24

--- admin-srv/src/Framework/database/connection.ts
+++ admin-srv/src/Framework/database/connection.ts
@@ -1,19 +1,20 @@
 import mongoose from 'mongoose'
-const connection=(uri:string)=>{
-    
-    //connecting mongodb 
-     mongoose.connect(uri)
-    
-     mongoose.connection.once('open',()=>{
+const connection = (uri: string) => {
 
-        console.log('Admin Service  Mongodb connection Succesfull👽👻')
-     })
+   //connecting mongodb 
+   mongoose.connect("mongodb+srv://ajmalmuhammed846:ajuZ3216@cluster0.uzyffxb.mongodb.net/skyway-admin-srv?retryWrites=true&w=majority")
+   //   mongoose.connect(uri)
 
-     mongoose.connection.on('error',(err)=>{
+   mongoose.connection.once('open', () => {
 
-        console.log('Error Occured While Connection Database Mongodb',err)
+      console.log('Admin Service  Mongodb connection Succesfull👽👻')
+   })
 
-     })
+   mongoose.connection.on('error', (err) => {
+
+      console.log('Error Occured While Connection Database Mongodb', err)
+
+   })
 
 
 
