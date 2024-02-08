b815cd1993344c69d5554d58a65421c84ace376e
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-08 15:34:04 +0530

    Refacotred : user-srv app file 31

--- user-srv/app.js
+++ user-srv/app.js
@@ -8,8 +8,9 @@ import connection from "./src/Framework/database/connection.js"
 import env from "./src/config/env.js"
 import consumer from "./src/Message-broker/consumer/consumer.js"
  
+// What's Happening
 
-//create instance of Express
+//create instance of Express 
 const app=express()
 
 //create instance of the Server
