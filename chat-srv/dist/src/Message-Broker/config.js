4d3da499c8f2a74388ab0e10ac71635fb93bf4fb
ajmal31 <ajmalmuhammed846@gmail.com>
2024-03-20 02:36:02 +0530

    refacotred

--- chat-srv/dist/src/Message-Broker/config.js
+++ chat-srv/dist/src/Message-Broker/config.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const amqplib_1 = __importDefault(require("amqplib"));
 const createChannel = async () => {
-    // const url="amqp://rabbitmq-service:5672"// service name
-    const url = 'amqp://localhost';
+    const url = "amqp://rabbitmq-service:5672"; // service name
+    // const url='amqp://localhost'   
     // const url='amqp://host.docker.internal:5672'   
     const connection = await amqplib_1.default.connect(url);
     const channel = await connection.createChannel();
